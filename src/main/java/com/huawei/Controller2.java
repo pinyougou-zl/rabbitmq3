322c8c398150b5c49796213dc78fddf86f027b71
CYC <386441042@qq.com>
2021-10-21 19:54:25 +0800

    11

--- src/main/java/com/huawei/Controller2.java
+++ src/main/java/com/huawei/Controller2.java
@@ -38,7 +38,7 @@ public class Controller2 {
         list.add("sp1===02===06=");
         list.add("sp111=======");
         list.add("sp2====04===");
- 		list.add("sp2===02==="+"0912");
+ 		list.add("sp2===02==="+"0912"+"02");
 
         list.add("sp2============");
         list.add("sp2=="+"1021");
