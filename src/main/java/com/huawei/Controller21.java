e746c5d2f180794c6b247eb5da4a9382de26f162
CYC <386441042@qq.com>
2021-11-25 07:38:53 +0800

    202111251

--- src/main/java/com/huawei/Controller21.java
+++ src/main/java/com/huawei/Controller21.java
@@ -1,13 +1,25 @@
 package com.huawei;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
-public class Controller2 {
-     @GetMapping("/test2")
+public class Controller21 {
+
+    @PostMapping("/test3")
+    public User test3(@RequestBody User user) {
+        user.setUserID(user.getUserID()+1000);
+        user.setName(user.getName()+"chener");
+        user.setAge(user.getAge()-10);
+        return user;
+    }
+
+
+     @PostMapping("/test2")
     public ArrayList test2() {
         ArrayList<Object> list = new ArrayList<>();
         list.add("zhangsan23===2");
