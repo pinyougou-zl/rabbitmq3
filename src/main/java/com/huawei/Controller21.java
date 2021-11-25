92b639844c3ef94f4ee0ffdefa732f55eccdf0f8
CYC <386441042@qq.com>
2021-11-25 08:03:36 +0800

    233232

---
+++ src/main/java/com/huawei/Controller21.java
@@ -0,0 +1,58 @@
+package com.huawei;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class Controller21 {
+
+    @PostMapping("/test3")
+    public User test3(@RequestBody User user) {
+        user.setUserID(user.getUserID()+1000);
+        user.setName(user.getName()+"chener");
+        user.setAge(user.getAge()-10);
+        return user;
+    }
+
+
+     @PostMapping("/test2")
+    public ArrayList test2() {
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("zhangsan23===2");
+        list.add("lisi23");
+        list.add("wangwu23");
+        list.add("sp1===022====");
+        list.add("sp1211=======");
+        list.add("sp22====04===2");
+        list.add("sp22==0912====03======");
+
+        list.add("sp22============");
+        list.add("sp22==0912====03===04===");
+        list.add("sp2==0912====03===04===");
+        list.add("sp2==0912====03===04===");
+        list.add("sp2==0912====03===04===");
+        list.add("sp2==0912====03===04===");
+        list.add("sp2==0912====03===04===");
+        list.add("sp22==0912====03===04===");
+        return list;
+    }
+
+    @GetMapping("/test1")
+    public ArrayList test1() {
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("zhangsan3");
+        list.add("lisi3");
+        list.add("wangwu3");
+        list.add("sp1===02====");
+        list.add("sp111=======");
+        list.add("sp2====04===");
+        list.add("sp2==0912====03======");
+
+        list.add("sp2============");
+        list.add("sp2==0912====03===04===");
+        return list;
+    }}
